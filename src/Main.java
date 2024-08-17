a033edcb936a9d9cf45f4051428ae178eedbc111
SteffenHub <steffenhub@users.noreply.github.com>
2024-08-17 21:15:18 +0200

    Documentation for main class.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+// project imports
 import order.DifferentOrderSIzeException;
 import order.Orders;
 import apriori.Apriori;
@@ -8,12 +9,13 @@ import args.ArgsInput;
 import args.ReadArgsException;
 import item.ItemSet;
 import order.WrongIndexForItemException;
-import org.sat4j.specs.ContradictionException;
-import org.sat4j.specs.TimeoutException;
-import satSolver.SatSolver;
 import txtImportExport.TxtConverter;
 import txtImportExport.TxtReaderWriter;
-
+import satSolver.SatSolver;
+// external imports
+import org.sat4j.specs.ContradictionException;
+import org.sat4j.specs.TimeoutException;
+// java imports
 import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
@@ -21,28 +23,46 @@ import java.util.*;
 
 public class Main {
 
+    /**
+     * The main method of the application.
+     * It reads the command line arguments, processes the orders and rules,
+     * performs Apriori algorithm to find all possible combinations,
+     * generates conclusions based on the combinations,
+     * and saves the results in text files.
+     *
+     * @param args the command line arguments. Examples can be found in ArgsInput class or its Exception class
+     * @throws IOException if there is an error reading or writing files
+     * @throws ContradictionException if there is a contradiction in the ruleset
+     * @throws TimeoutException if the satSolver computation exceeds the time limit
+     * @throws ReadArgsException if there are not enough command line arguments
+     * @throws DifferentOrderSIzeException if the orders have different sizes
+     * @throws WrongIndexForItemException if an item has an invalid index
+     */
     public static void main(String[] args) throws IOException, ContradictionException, TimeoutException, ReadArgsException, DifferentOrderSIzeException, WrongIndexForItemException {
 
-        //read args
+        // read args
         ArgsInput argsInput = new ArgsInput(args);
 
+        // start timer
         Instant start = Instant.now();
 
-        List<int[]> regelWerk = TxtConverter.stringListToListOfIntArrays(TxtReaderWriter.getTxtFromSamePath(argsInput.getRuleFile()));
-        SatSolver satSolver = new SatSolver(regelWerk.toArray(new int[0][]));
+        // Read the Ruleset and initiate the SatSolver with it
+        List<int[]> ruleSet = TxtConverter.stringListToListOfIntArrays(TxtReaderWriter.getTxtFromSamePath(argsInput.getRuleFile()));
+        SatSolver satSolver = new SatSolver(ruleSet.toArray(new int[0][]));
 
+        // Read the order file and initiate the Orders object with it
         List<String> readOrders = TxtReaderWriter.getTxtFromSamePath(argsInput.getOrderFile());
         boolean[][] ordersBool = TxtConverter.listOfStringOrdersToBooleanArray(readOrders);
-
         Orders orders = new Orders(ordersBool);
-        Apriori apriori = new Apriori(satSolver, orders, argsInput);
-        HashMap<ItemSet, Double> allPossibleKombinationFiltered = apriori.run();
 
+        // initiate Apriori and start the combination calculation
+        Apriori apriori = new Apriori(satSolver, orders, argsInput);
+        HashMap<ItemSet, Double> allPossibleCombinationFiltered = apriori.run();
 
         //save to directory
-        List<String> allPossibleKombinationFilteredString = new ArrayList<>();
-        for (ItemSet kombi: allPossibleKombinationFiltered.keySet()) {
-            allPossibleKombinationFilteredString.add(kombi + " " + allPossibleKombinationFiltered.get(kombi));
+        List<String> allPossibleCombinationFilteredString = new ArrayList<>();
+        for (ItemSet combination: allPossibleCombinationFiltered.keySet()) {
+            allPossibleCombinationFilteredString.add(combination + " " + allPossibleCombinationFiltered.get(combination));
         }
         TxtReaderWriter.writeListOfStrings("allPossibleCombination_Apriori_"
                         + argsInput.getRuleFileWithoutPath() + "_"
@@ -50,9 +70,10 @@ public class Main {
                         + argsInput.getMinSupport() + "_"
                         + argsInput.getMinConfidence() + "_"
                         + argsInput.getDepth()
-                        + ".txt", allPossibleKombinationFilteredString);
+                        + ".txt", allPossibleCombinationFilteredString);
 
-        Conclusion[] allConclusions = new ConclusionBuilder(allPossibleKombinationFiltered).run();
+        // generate the conclusions with all the possible combinations found in Apriori process
+        Conclusion[] allConclusions = new ConclusionBuilder(allPossibleCombinationFiltered).run();
         allConclusions = new ConfidenceBuilder(allConclusions, orders, argsInput).run();
 
         //save to directory
@@ -68,6 +89,7 @@ public class Main {
                         + argsInput.getDepth()
                         + ".txt", allConclusionsString);
 
+        // Print the needed time
         Instant end = Instant.now();
         Duration interval = Duration.between(start, end);
         System.out.println("time for calculation: " + interval.getSeconds() + "sec");
